f4ce1ab56122acdd99cf65165e123cdc83413c1b
Fedor Eronin <eroninf@icloud.com>
2022-07-23 19:33:55 +0300

    add inner shadow

--- Weather/Views/Detail/ForecastView.swift
+++ Weather/Views/Detail/ForecastView.swift
@@ -15,6 +15,10 @@ struct ForecastView: View {
         .background(Color.bottomSheetBackground)
         .backgroundBlur(radius: 25, opaque: true)
         .clipShape(RoundedRectangle(cornerRadius: 44))
+        .innerShadow(shape: RoundedRectangle(cornerRadius: 44),
+                     color: Color.bottomSheetBorderMiddle, lineWidth: 1, offsetX: 0,
+                     offsetY: 1, blur: 0, blendMode: .overlay, opacity: 1)
+        
         .overlay {
             Divider()
                 .blendMode(.overlay)
