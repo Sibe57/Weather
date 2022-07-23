92dd1bad72a590d2c18dd241ae251d1b936fc570
Fedor Eronin <eroninf@icloud.com>
2022-07-23 15:26:18 +0300

    setup background views

---
+++ Weather/Views/Main/HomeView.swift
@@ -0,0 +1,63 @@
+//
+//  HomeView.swift
+//  Weather
+//
+//  Created by Федор Еронин on 23.07.2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.background
+                .ignoresSafeArea()
+            
+            Image("Background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            Image("House")
+                .frame(maxHeight: .infinity, alignment: .top)
+                .padding(.top, 257)
+            
+            VStack(spacing: -10) {
+                Text("Montreal")
+                    .font(.largeTitle)
+                VStack {
+                    Text(attributedString)
+                    Text("H:24° L:18°")
+                        .font(.title3.weight(.semibold))
+                }
+                
+                Spacer()
+            }
+            .padding(.top, 51
+            )
+        }
+    }
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n" + "Mostly Clear")
+        
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+        }
+        if let pipe = string.range(of: " | ") {
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        if let weather = string.range(of: "Mostly Clear") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        return string
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .preferredColorScheme(.dark)
+    }
+}
